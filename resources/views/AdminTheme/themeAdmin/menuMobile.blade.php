42c5e822a05b720d2d78a1a1ddef10c58f9c2063
Daniel Garcia Diaz <daniel@MacBook-Air-de-Daniel.local>
2018-06-20 14:07:21 +0200

    Menu movil

---
+++ resources/views/AdminTheme/themeAdmin/menuMobile.blade.php
@@ -0,0 +1,32 @@
+<!-- start: Mobile -->
+<div id="mimin-mobile" class="reverse">
+    <div class="mimin-mobile-menu-list">
+        <div class="col-md-12 sub-mimin-mobile-menu-list animated fadeInLeft">
+            <ul class="nav nav-list">
+                <li class="active ripple">
+                    <a class="tree-toggle nav-header">
+                        <span class="fa-home fa"></span>Menú Principal
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    </a>
+                    <ul class="nav nav-list tree">
+                        <li><a href="gestion">Administración</a></li>
+                    </ul>
+                </li>
+            
+                <li class="ripple">
+                    <a href="calendario">
+                        <span class="fa fa-calendar-o"></span>Calendario
+                    </a>
+                </li>
+                
+               
+               
+                <li><a href="creditos">Créditos</a></li>
+            </ul>
+        </div>
+    </div>       
+</div>
+<button id="mimin-mobile-menu-opener" class="animated rubberBand btn btn-circle btn-danger">
+    <span class="fa fa-bars"></span>
+</button>
+<!-- end: Mobile -->
